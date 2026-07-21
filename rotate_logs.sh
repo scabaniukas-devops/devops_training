ec01b05c78264bc84049eb50fb2821a119f4727a
test_git <chab26@gmail.com>
2026-07-21 12:17:06 +0300

    feat: implement automatic log rotation and archiving

--- rotate_logs.sh
+++ rotate_logs.sh
@@ -1,15 +1,38 @@
-!/bin/bash
+#!/bin/bash
 
-for file in app_*.log
-do
-	if [ -s "$file" ]; then
+# Path to our working folder and log file
+LOG_DIR="/home/ubuntu/devops_training/scripts"
+LOG_FILE="$LOG_DIR/disk_monitor.log"
+TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
+ARCHIVE_NAME="$LOG_DIR/disk_monitor_$TIMESTAMP.tar.gz"
 
-		mv "$file" "$file.old"
+echo "=== Starting log rotation: $(date) ==="
 
-		touch "$file"
+# 1. Check if the log file exists and is not empty.
+if [ -s "$LOG_FILE" ]; then
+    echo "Log file found. Starting archiving...."
 
-		echo " File $file have logs ... Geting archive ..."
-	else
-		echo "The File $file is empty"
-	fi
-done 
+    # 2. Creating a compressed archive using an archiver. tar
+    # Flags: -c (create), -z (gzip compression), -f (file)
+    tar -czf "$ARCHIVE_NAME" -C "$LOG_DIR" disk_monitor.log
+
+    if [ $? -eq 0 ]; then
+        echo "Archive successfully created: $ARCHIVE_NAME"
+
+        # 3. Clear the original log file so that check_disk.sh can write to it again
+        # Redirection (>) without a command simply empties the file while preserving access permissions
+        > "$LOG_FILE"
+        echo "The original log file has been cleared.."
+    else
+        echo "Error: Failed to create the archive!"
+        exit 1
+    fi
+else
+    echo "The log file is empty or does not exist. Rotation is not required."
+fi
+
+# 4. Delete archives older than 7 days so they don't accumulate indefinitely
+# Find files matching the pattern with a modification time (+mtime) older than 7 days and delete them (-delete)
+find "$LOG_DIR" -name "disk_monitor_*.tar.gz" -mtime +7 -delete
+
+echo "=== The rotation has been successfully completed. ==="
